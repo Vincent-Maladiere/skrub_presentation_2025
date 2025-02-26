cc66cec7fa1ed937311dd2d577b50fd39e946488
Gael Varoquaux <gael.varoquaux@normalesup.org>
2025-02-26 13:50:40 +0100

    Pipeline tuning

--- 04_assembling.py
+++ 04_assembling.py
@@ -13,12 +13,10 @@ from skrub.datasets import fetch_credit_fraud
 
 dataset = fetch_credit_fraud()
 # A first table, linking baskets to fraud
-baskets = dataset.baskets
-skrub.TableReport(baskets)
+skrub.TableReport(dataset.baskets)
 # %%
 # A second table, which gives the products in each basket
-products = dataset.products
-skrub.TableReport(products)
+skrub.TableReport(dataset.products)
 # %%
 # We need to 1) group the products by basket, 2) join the two tables
 #
@@ -28,8 +26,9 @@ next(iter(dataset.products.groupby('basket_ID')))[1]
 # %%
 # A groupby calls for an aggregation. How to aggregate the items, models: strings?
 # We'll vectorize the table
-vectorizer = skrub.TableVectorizer(high_cardinality=skrub.StringEncoder())
-vectorized_products = vectorizer.fit_transform(products)
+vectorizer = skrub.TableVectorizer(high_cardinality=skrub.StringEncoder(),
+                                   n_jobs=-1)
+vectorized_products = vectorizer.fit_transform(dataset.products)
 
 # %%
 # We can now aggregate the products and join the tables: pandas operations
@@ -38,7 +37,7 @@ aggregated_products = (
 )
 aggregated_products
 # %%
-baskets = baskets.merge(
+baskets = dataset.baskets.merge(
     aggregated_products, left_on="ID", right_on="basket_ID"
 ).drop(columns=["ID", "basket_ID"])
 baskets
@@ -47,16 +46,20 @@ baskets
 # And we can now train a model
 y = baskets['fraud_flag']
 X = baskets.drop('fraud_flag', axis=1)
-from sklearn.ensemble import RandomForestClassifier
-model = RandomForestClassifier(n_jobs=-1)
+from sklearn.ensemble import ExtraTreesClassifier # It's fast
+model = ExtraTreesClassifier(n_jobs=-1)
 from sklearn.model_selection import cross_val_score
 cross_val_score(model, X, y, scoring='roc_auc')
+
+
 # %%
 # But we are back to the same problem as in the first section:
 # We're in pandas' land. When comes new data, how to apply the same transformations?
 # How to cross-validate, or tune the data-preparation steps?
 
 
+
+#######################################################################
 # skrub comes with a way to change ever so slightly what we did above
 # We define our inputs as "variables"
 products = skrub.var("products", dataset.products)
@@ -83,7 +86,7 @@ baskets = baskets.drop(columns=["ID", "basket_ID"])
 baskets
 # %%
 # And we do the prediction
-predictions = baskets.skb.apply(RandomForestClassifier(), y=fraud_flags)
+predictions = baskets.skb.apply(ExtraTreesClassifier(n_jobs=-1), y=fraud_flags)
 predictions
 
 # %%
@@ -92,20 +95,21 @@ predictions
 
 # For instance, let's optimize a bit our vectorization of the products.
 # We just need to change a bit the above code
-encoder = skrub.choose_from(
-    {
-        "MinHash": skrub.MinHashEncoder(),
-        "LSA": skrub.StringEncoder(),
-    },
-    name="encoder",
+encoder = skrub.StringEncoder(
+    vectorizer=skrub.choose_from(["tfidf", "hashing"], name="vectorizer"),
 )
-vectorizer = skrub.TableVectorizer(high_cardinality=encoder)
+vectorizer = skrub.TableVectorizer(high_cardinality=encoder,
+                                   n_jobs=2)
 vectorized_products = products.skb.apply(vectorizer, cols=s.all() - "basket_ID")
 aggregated_products = vectorized_products.groupby("basket_ID").agg("mean").reset_index()
+
+# We redefine our sources, to have a clean start
+baskets = skrub.var("baskets", dataset.baskets[["ID"]]).skb.mark_as_x()
 baskets = baskets.merge(aggregated_products, left_on="ID", right_on="basket_ID")
 baskets = baskets.drop(columns=["ID", "basket_ID"])
-predictions = baskets.skb.apply(RandomForestClassifier(), y=fraud_flags)
+predictions = baskets.skb.apply(ExtraTreesClassifier(n_jobs=-1), y=fraud_flags)
 
-search = predictions.skb.get_grid_search(n_jobs=-1)
+search = predictions.skb.get_grid_search(fitted=True, scoring="roc_auc",
+                                         verbose=True)
 search.get_cv_results_table()
 # %%
