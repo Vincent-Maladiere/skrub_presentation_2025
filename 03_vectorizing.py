43228667f9fdf57a6b0e20755a1f470c7f67a49a
Vincent Maladiere <maladiere.vincent@yahoo.fr>
2025-07-02 15:48:13 +0200

    fix typos and broken part of the skrub api

--- 03_vectorizing.py
+++ 03_vectorizing.py
@@ -5,48 +5,48 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('employees_salaries.csv')
-y = df['salary']
-df = df.drop('salary', axis=1)
+df = pd.read_csv("employees_salaries.csv")
+y = df["salary"]
+df = df.drop("salary", axis=1)
 
-results = {} # We will store the results here
+results = {}  # We will store the results here
 # %%
 # The default TableVectorizer
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_validate
 from skrub import TableVectorizer
+
 model = make_pipeline(TableVectorizer(), RandomForestRegressor())
-results[model] = cross_validate(model, df, y, scoring='r2', n_jobs=-1)
-print(f"R2 score {np.mean(results[model]['test_score']):.2f} in {np.mean(results[model]['fit_time']):.2f} seconds")
+results[model] = cross_validate(model, df, y, scoring="r2", n_jobs=-1)
+print(
+    f"R2 score {np.mean(results[model]['test_score']):.2f} in {np.mean(results[model]['fit_time']):.2f} seconds"
+)
 model
 
 # %%
 # The default tabular_learner
 from skrub import tabular_learner
-model = tabular_learner(RandomForestRegressor())
-results[model] = cross_validate(model, df, y, scoring='r2', n_jobs=-1)
-print(f"R2 score {np.mean(results[model]['test_score']):.2f} in {np.mean(results[model]['fit_time']):.2f} seconds")
-model
 
-# %%
-# Let us vary the high_cardinality parameter
-from skrub import StringEncoder
-model['tablevectorizer'].high_cardinality = StringEncoder()
-results[model] = cross_validate(model, df, y, scoring='r2', n_jobs=-1)
-print(f"R2 score {np.mean(results[model]['test_score']):.2f} in {np.mean(results[model]['fit_time']):.2f} seconds")
+model = tabular_learner(RandomForestRegressor())
+results[model] = cross_validate(model, df, y, scoring="r2", n_jobs=-1)
+print(
+    f"R2 score {np.mean(results[model]['test_score']):.2f} in {np.mean(results[model]['fit_time']):.2f} seconds"
+)
 model
 
 # %%
 # And if we want, we can use LLMs to embed the high cardinality strings
 # Only 2 cores, to avoid blowing up the memory
 from skrub import TextEncoder
-model['tablevectorizer'].high_cardinality = TextEncoder()
-results[model] = cross_validate(model, df, y, scoring='r2', n_jobs=-2)
-print(f"R2 score {np.mean(results[model]['test_score']):.2f} in {np.mean(results[model]['fit_time']):.2f} seconds")
+
+model["tablevectorizer"].high_cardinality = TextEncoder()
+results[model] = cross_validate(model, df, y, scoring="r2", n_jobs=-2)
+print(
+    f"R2 score {np.mean(results[model]['test_score']):.2f} in {np.mean(results[model]['fit_time']):.2f} seconds"
+)
 model
 
 # Beyond the string encoders, we also have the DatetimeEncoder
 
 # %%
-
--- 04_assembling.py
+++ 04_assembling.py
@@ -21,20 +21,17 @@ skrub.TableReport(dataset.products)
 # We need to 1) group the products by basket, 2) join the two tables
 #
 # An example of basket looks like this
-next(iter(dataset.products.groupby('basket_ID')))[1]
+next(iter(dataset.products.groupby("basket_ID")))[1]
 
 # %%
 # A groupby calls for an aggregation. How to aggregate the items, models: strings?
 # We'll vectorize the table
-vectorizer = skrub.TableVectorizer(high_cardinality=skrub.StringEncoder(),
-                                   n_jobs=-1)
+vectorizer = skrub.TableVectorizer(high_cardinality=skrub.StringEncoder(), n_jobs=-1)
 vectorized_products = vectorizer.fit_transform(dataset.products)
 
 # %%
 # We can now aggregate the products and join the tables: pandas operations
-aggregated_products = (
-   vectorized_products.groupby("basket_ID").agg("mean").reset_index()
-)
+aggregated_products = vectorized_products.groupby("basket_ID").agg("mean").reset_index()
 aggregated_products
 # %%
 baskets = dataset.baskets.merge(
@@ -44,19 +41,20 @@ baskets
 
 # %%
 # And we can now train a model
-y = baskets['fraud_flag']
-X = baskets.drop('fraud_flag', axis=1)
-from sklearn.ensemble import ExtraTreesClassifier # It's fast
+y = baskets["fraud_flag"]
+X = baskets.drop("fraud_flag", axis=1)
+from sklearn.ensemble import ExtraTreesClassifier  # It's fast
+
 model = ExtraTreesClassifier(n_jobs=-1)
 from sklearn.model_selection import cross_val_score
-cross_val_score(model, X, y, scoring='roc_auc')
+
+cross_val_score(model, X, y, scoring="roc_auc")
 
 
 # %%
 # But we are back to the same problem as in the first section:
 # We're in pandas' land. When comes new data, how to apply the same transformations?
 # How to cross-validate, or tune the data-preparation steps?
-
 
 
 #######################################################################
@@ -66,14 +64,15 @@ products = skrub.var("products", dataset.products)
 products
 # %%
 # Now we define our "x" and "y" variables
-baskets = skrub.var('baskets', dataset.baskets)
-basket_IDs = baskets[["ID"]].skb.mark_as_x()
+baskets = skrub.var("baskets", dataset.baskets)
+basket_IDs = baskets[["ID"]].skb.mark_as_X()
 fraud_flags = baskets["fraud_flag"].skb.mark_as_y()
 
 # %%
 # We can now proceed almost as above to prepare the data
 # We vectorize the products, with a slightly different API
 from skrub import selectors as s
+
 vectorized_products = products.skb.apply(vectorizer, cols=s.all() - "basket_ID")
 vectorized_products
 # %%
@@ -96,20 +95,22 @@ predictions
 
 # We load the test data
 data_test = fetch_credit_fraud(split="test")
-y_test = data_test.baskets['fraud_flag']
-basket_test = data_test.baskets.drop('fraud_flag', axis=1)
+y_test = data_test.baskets["fraud_flag"]
+basket_test = data_test.baskets.drop("fraud_flag", axis=1)
 
 # We can apply a predictor to this new data
-predictor = predictions.skb.get_estimator(fitted=True)
-y_pred = predictor.predict({
-    'baskets': basket_test,
-    'products': data_test.products,
-})
+predictor = predictions.skb.get_pipeline(fitted=True)
+y_pred = predictor.predict(
+    {
+        "baskets": basket_test,
+        "products": data_test.products,
+    }
+)
 
 # And we can evaluate the predictions
 from sklearn.metrics import classification_report
-print(classification_report(y_test, y_pred))
 
+print(classification_report(y_test, y_pred))
 
 
 # %%
@@ -118,29 +119,30 @@ print(classification_report(y_test, y_pred))
 encoder = skrub.StringEncoder(
     vectorizer=skrub.choose_from(["tfidf", "hashing"], name="vectorizer"),
 )
-vectorizer = skrub.TableVectorizer(high_cardinality=encoder,
-                                   n_jobs=2)
+vectorizer = skrub.TableVectorizer(high_cardinality=encoder, n_jobs=2)
 
 # The rest of the code remains the same
 vectorized_products = products.skb.apply(vectorizer, cols=s.all() - "basket_ID")
 aggregated_products = vectorized_products.groupby("basket_ID").agg("mean").reset_index()
 
 # We redefine our sources, to have a clean start
-baskets = skrub.var('baskets', dataset.baskets)
-basket_IDs = baskets[["ID"]].skb.mark_as_x()
+baskets = skrub.var("baskets", dataset.baskets)
+basket_IDs = baskets[["ID"]].skb.mark_as_X()
 fraud_flags = baskets["fraud_flag"].skb.mark_as_y()
 features = basket_IDs.merge(aggregated_products, left_on="ID", right_on="basket_ID")
 features = features.drop(columns=["ID", "basket_ID"])
 predictions = features.skb.apply(ExtraTreesClassifier(n_jobs=-1), y=fraud_flags)
 
-search = predictions.skb.get_grid_search(fitted=True, scoring="roc_auc",
-                                         verbose=2)
-search.get_cv_results_table()
+search = predictions.skb.get_grid_search(
+    fitted=True, scoring="roc_auc", verbose=2, cv=3
+)
+# %%
+search._get_cv_results_table()
 
 # %%
 # skrub gives you all kinds of tools to tune and inspect this pipeline:
 # For instance, we can visualize the hyperparameters selection
-search.plot_parallel_coord()
+search.plot_results()
 
 # %%
 # We can also get a full report of the pipeline
